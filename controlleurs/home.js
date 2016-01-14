75cf82275fb9665f3801969bde716843d99ba06e
powolnymarcel <powolnymarcel@gmail.com>
2016-01-14 04:32:52 +0100

    mise en place home ctrl

--- controlleurs/home.js
+++ controlleurs/home.js
@@ -1,5 +1,41 @@
 module.exports = {
 	index: function(req, res) {
-		res.render('index');
+		var viewModel = {
+			images: [
+				{
+					uniqueId: 1,
+					titre: 'Sample Image 1',
+					description: '',
+					filename: 'sample1.jpg',
+					vues: 0,
+					likes: 0,
+					timestamp: Date.now
+				}, {
+					uniqueId: 2,
+					titre: 'Sample Image 2',
+					description: '',
+					filename: 'sample2.jpg',
+					vues: 0,
+					likes: 0,
+					timestamp: Date.now
+				}, {
+					uniqueId: 3,
+					titre: 'Sample Image 3',
+					description: '',
+					filename: 'sample3.jpg',
+					vues: 0,
+					likes: 0,
+					timestamp: Date.now
+				}, {uniqueId: 4,
+					titre: 'Sample Image 4',
+					description: '',
+					filename: 'sample4.jpg',
+					vues: 0,
+					likes: 0,
+					timestamp: Date.now
+				}
+			]
+		};
+		res.render('index', viewModel);
 	}
 };
